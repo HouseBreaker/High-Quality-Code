c13f7ac812edca8d3ae1465729650fd39536ad99
Housey <housebreaker1@gmail.com>
2016-01-11 23:00:55 +0200

    Update StringExtensions by adding a Main class

---
+++ 03. Code Documentation and Comments/01. String Extensions/StringExtensionsMain.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.String_Extensions
+{
+	using System;
+	using System.Text;
+
+	public static class StringExtensionsMain
+	{
+		public static void Main()
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			const string Gosho = "гошо";
+
+			Console.WriteLine(Gosho.ConvertCyrillicToLatinLetters());
+		}
+	}
+}
