c24002af5c7e5bf7ae91e8b52859f9077efad65b
Housey <housebreaker1@gmail.com>
2016-01-29 23:22:12 +0200

    finish homework

---
+++ 10. Refactoring/ConsoleWrapper.cs
@@ -0,0 +1,36 @@
+﻿namespace RotatingMatrix
+{
+	using System;
+
+	public class ConsoleWrapper
+	{
+		private static void Main()
+		{
+			Console.Write("Enter a positive number: ");
+			string input = Console.ReadLine();
+
+			int n;
+			while (!int.TryParse(input, out n))
+			{
+				input = Console.ReadLine();
+			}
+
+			int[,] matrix = MatrixGenerator.GenerateRotationalWalkInMatrix(n);
+
+			PrintMatrix(matrix);
+		}
+
+		private static void PrintMatrix(int[,] matix)
+		{
+			for (int row = 0; row < matix.GetLength(0); row++)
+			{
+				for (int col = 0; col < matix.GetLength(1); col++)
+				{
+					Console.Write("{0,4}", matix[row, col]);
+				}
+
+				Console.WriteLine();
+			}
+		}
+	}
+}
---
+++ 10. Refactoring/WalkInMatrixTests/RotatingMatrixTest.cs
@@ -0,0 +1,130 @@
+﻿namespace WalkInMatrixTests
+{
+	using System.Text;
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	using RotatingMatrix;
+
+	[TestClass]
+	public class RotatingMatrixTest
+	{
+		[TestMethod]
+		public void TestOutputMatrixWith4()
+		{
+			int n = 4;
+			int[,] expectedMatrix =
+				{
+					{ 1, 10, 11, 12 }, 
+					{ 9, 2, 15, 13 }, 
+					{ 8, 16, 3, 14 }, 
+					{ 7, 6, 5, 4 }
+				};
+
+			string expected = this.MatrixAsString(expectedMatrix);
+			int[,] actualMatrix = MatrixGenerator.GenerateRotationalWalkInMatrix(n);
+			string actual = this.MatrixAsString(actualMatrix);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TestOutputMatrixWith5()
+		{
+			int n = 5;
+			int[,] expectedMatrix =
+				{
+					{ 1, 13, 14, 15, 16 }, 
+					{ 12, 2, 21, 22, 17 }, 
+					{ 11, 23, 3, 20, 18 }, 
+					{ 10, 25, 24, 4, 19 }, 
+					{ 9, 8, 7, 6, 5 }
+				};
+
+			int[,] actualMatrix = MatrixGenerator.GenerateRotationalWalkInMatrix(n);
+
+			string expected = this.MatrixAsString(expectedMatrix);
+			string actual = this.MatrixAsString(actualMatrix);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TestOutputMatrixWith7()
+		{
+			int n = 7;
+			int[,] expectedMatrix =
+				{
+					{ 1, 19, 20, 21, 22, 23, 24 }, 
+					{ 18, 2, 33, 34, 35, 36, 25 }, 
+					{ 17, 40, 3, 32, 39, 37, 26 }, 
+					{ 16, 48, 41, 4, 31, 38, 27 }, 
+					{ 15, 47, 49, 42, 5, 30, 28 }, 
+					{ 14, 46, 45, 44, 43, 6, 29 }, 
+					{ 13, 12, 11, 10, 9, 8, 7 }
+				};
+
+			string expected = this.MatrixAsString(expectedMatrix);
+			int[,] actualMatrix = MatrixGenerator.GenerateRotationalWalkInMatrix(n);
+			string actual = this.MatrixAsString(actualMatrix);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TestInvalidOutputMatrixWith7()
+		{
+			int n = 7;
+			int[,] expectedMatrix =
+				{
+					{ 1, 19, 20, 21, 22, 23, 24 }, 
+					{ 18, 2, 33, 34, 35, 36, 25 }, 
+					{ 17, 39, 3, 32, 39, 37, 26 }, 
+					{ 16, 48, 41, 4, 31, 38, 27 }, 
+					{ 15, 47, 49, 42, 5, 30, 28 }, 
+					{ 14, 46, 45, 44, 43, 6, 29 }, 
+					{ 13, 12, 11, 10, 9, 8, 7 }
+				};
+
+			string expected = this.MatrixAsString(expectedMatrix);
+			int[,] actualMatrix = MatrixGenerator.GenerateRotationalWalkInMatrix(n);
+			string actual = this.MatrixAsString(actualMatrix);
+			Assert.AreNotEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TestInvalidOutputMatrixWith7InFirstPosition()
+		{
+			int n = 7;
+			int[,] expectedMatrix =
+				{
+					{ 50, 19, 20, 21, 22, 23, 24 }, 
+					{ 18, 2, 33, 34, 35, 36, 25 }, 
+					{ 17, 40, 3, 32, 39, 37, 26 }, 
+					{ 16, 48, 41, 4, 31, 38, 27 }, 
+					{ 15, 47, 49, 42, 5, 30, 28 }, 
+					{ 14, 46, 45, 44, 43, 6, 29 }, 
+					{ 13, 12, 11, 10, 9, 8, 7 }
+				};
+
+			string expected = this.MatrixAsString(expectedMatrix);
+			int[,] actualMatrix = MatrixGenerator.GenerateRotationalWalkInMatrix(n);
+			string actual = this.MatrixAsString(actualMatrix);
+			Assert.AreNotEqual(expected, actual);
+		}
+
+		private string MatrixAsString(int[,] matrix)
+		{
+			StringBuilder result = new StringBuilder();
+
+			for (int row = 0; row < matrix.GetLength(0); row++)
+			{
+				for (int col = 0; col < matrix.GetLength(1); col++)
+				{
+					result.Append(matrix[row, col] + " ");
+				}
+			}
+
+			result.Length--;
+			return result.ToString();
+		}
+	}
+}
